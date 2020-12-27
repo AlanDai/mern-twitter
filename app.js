96e1b8567911d8a1bd1731230ec3e9074d815cae
alanxudai@gmail.com <alanxudai@gmail.com>
2020-12-26 19:05:31 -0800

    update configs for heroku deploy

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
